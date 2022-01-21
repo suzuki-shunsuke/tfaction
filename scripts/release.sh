679c3dbe8856526b4e0be0c57acb07bf1369d272
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-01-21 21:03:33 +0900

    chore: add a base revision to the release commit

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,14 @@ set -o pipefail
 
 version=$1
 
+sha=$(git rev-parse HEAD)
 git ls-files | grep -E "/action\.yaml$" |
   xargs -n 1 sed -i "s|- uses: suzuki-shunsuke/tfaction/\(.*\)@main|- uses: suzuki-shunsuke/tfaction/\1@$version|"
 
 git checkout -b "release/$version"
 git ls-files | grep -E "/action\.yaml$" | xargs git add
-git commit -m "chore: release $version"
+git commit -m "chore: release $version
+base revision: $sha"
 git tag "$version"
 git push origin "$version"
 git checkout main
