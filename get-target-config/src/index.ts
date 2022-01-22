93cb5ab5a8b0ef0ccd919b048c2bff74ce3c43dd
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-01-22 16:48:57 +0900

    fix: add the prefix "aws_" to "assume_role_arn"

--- get-target-config/src/index.ts
+++ get-target-config/src/index.ts
@@ -15,7 +15,7 @@ try {
       continue;
     }
     core.setOutput('working_directory', target.replace(targetConfig.target, targetConfig.working_directory));
-    core.setOutput('assume_role_arn', isApply ? targetConfig.assume_role_arns.terraform_apply : targetConfig.assume_role_arns.terraform_plan);
+    core.setOutput('aws_assume_role_arn', isApply ? targetConfig.aws_assume_role_arns.terraform_apply : targetConfig.aws_assume_role_arns.terraform_plan);
     core.setOutput('aws_region', targetConfig.aws_region);
     core.setOutput('s3_bucket_name_plan_file', targetConfig.s3_bucket_name_plan_file);
     core.setOutput('s3_bucket_name_tfmigrate_history', targetConfig.s3_bucket_name_tfmigrate_history);
