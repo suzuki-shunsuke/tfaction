1a2271871289122f22e2f97408128cfa7f78611b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-03-02 08:47:57 +0900

    chore: create release

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,3 +16,7 @@ base revision: $sha"
 git tag "$version"
 git push origin "$version"
 git checkout main
+note_template='[Issues](https://github.com/suzuki-shunsuke/tfaction/issues?q=is%3Aissue+is%3Aclosed+milestone%3AVERSION) | [Pull Requests](https://github.com/suzuki-shunsuke/tfaction/pulls?q=is%3Apr+is%3Aclosed+milestone%3AVERSION) | https://github.com/suzuki-shunsuke/tfaction/compare/VERSION...VERSION | [Base revision](https://github.com/suzuki-shunsuke/tfaction/tree/BASE_REVISION)'
+note_1=${note_template//VERSION/$version}
+note=${note_1//BASE_REVISION/$sha}
+gh release create "$version" -p --title "$version" -n "${note}"
