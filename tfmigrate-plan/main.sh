8aa5870808a416581bc22189b20069d5916fd72c
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2022-08-26 21:08:58 +0900

    Apply suggestions from code review

--- tfmigrate-plan/main.sh
+++ tfmigrate-plan/main.sh
@@ -12,11 +12,11 @@ if [ -n "${GCS_BUCKET_NAME_PLAN_FILE:-}" ]; then
 fi
 
 if [ ! -f .tfmigrate.hcl ]; then
-	if [ -n "${S3_BUCKET_NAME_PLAN_FILE:-}" ]; then
+	if [ -n "${S3_BUCKET_NAME_TFMIGRATE_HISTORY:-}" ]; then
 		sed "s|%%TARGET%%|$TFACTION_TARGET|g" \
 			"$GITHUB_ACTION_PATH/tfmigrate.hcl" |
 			sed "s|%%S3_BUCKET_NAME_TFMIGRATE_HISTORY%%|$S3_BUCKET_NAME_TFMIGRATE_HISTORY|g" >.tfmigrate.hcl
-	elif [ -n "${GCS_BUCKET_NAME_PLAN_FILE:-}" ]; then
+	elif [ -n "${GCS_BUCKET_NAME_TFMIGRATE_HISTORY:-}" ]; then
 		sed "s|%%TARGET%%|$TFACTION_TARGET|g" \
 			"$GITHUB_ACTION_PATH/tfmigrate-gcs.hcl" |
 			sed "s|%%GCS_BUCKET_NAME_TFMIGRATE_HISTORY%%|$GCS_BUCKET_NAME_TFMIGRATE_HISTORY|g" >.tfmigrate.hcl
