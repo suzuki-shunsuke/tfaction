72524b9366348efe102ae869be1dc821477948d8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-09-14 19:47:10 +0900

    feat(tfmigrate-plan): support testing with Conftest

--- tfmigrate-plan/main.sh
+++ tfmigrate-plan/main.sh
@@ -40,4 +40,14 @@ github-comment exec \
 	--config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
 	-k tfmigrate-plan \
 	-var "tfaction_target:$TFACTION_TARGET" \
-	-- tfmigrate plan
+	-- tfmigrate plan --out tfplan.binary
+
+if [ -d "$ROOT_DIR/policy" ]; then
+	github-comment exec -- terraform show -json tfplan.binary >tfplan.json
+	conftest -v # Install conftest in advance to exclude aqua lazy install log from github-comment's comment
+	github-comment exec \
+		--config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
+		-var "tfaction_target:$TFACTION_TARGET" \
+		-k conftest -- \
+			conftest test --no-color -p "$ROOT_DIR/policy" tfplan.json
+fi
