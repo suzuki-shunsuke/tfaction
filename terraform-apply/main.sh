5b0102f24fcc95b090f39ee0f61bab45acf36f2b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-03-13 23:03:54 +0900

    fix(terraform-apply): fix the usage of wc

--- terraform-apply/main.sh
+++ terraform-apply/main.sh
@@ -23,7 +23,7 @@ set -e
 prs_txt=$(mktemp)
 github-comment exec -config "${GITHUB_ACTION_PATH}/github-comment.yaml" -var "tfaction_target:$TFACTION_TARGET" -- gh pr list --json number -L 100 -l "$TFACTION_TARGET" -q ".[].number" | tee "$prs_txt"
 
-if [ "$UPDATE_ONLY_ONE_PR" = "true" ] && [ "$(wc -l "$prs_txt")" -gt 1 ]; then
+if [ "$UPDATE_ONLY_ONE_PR" = "true" ] && [ "$(wc -l < "$prs_txt")" -gt 1 ]; then
 	head -n 1 "$prs_txt" | bash "${GITHUB_ACTION_PATH}/update_branch.sh"
 	while read -r pr_number; do
 		if [ "$CI_INFO_PR_NUMBER" = "$pr_number" ]; then
