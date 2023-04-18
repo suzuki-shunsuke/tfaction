74053df86cc68eabc1b7b5c092ba639f03aa997e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-04-18 17:58:41 +0900

    fix: fix env names

--- terraform-plan/update_drift_issue.sh
+++ terraform-plan/update_drift_issue.sh
@@ -3,12 +3,12 @@
 set -eu
 set -o pipefail
 
-if [ "$ISSUE_STATE" = open ] && [ "$code" -eq 0 ]; then
+if [ "$TFACTION_DRIFT_ISSUE_STATE" = open ] && [ "$code" -eq 0 ]; then
 	gh issue close \
-		--repo "$ISSUE_REPO_OWNER/$ISSUE_REPO_NAME" \
-		"$ISSUE_NUMBER"
-elif [ "$ISSUE_STATE" = closed ] && [ "$code" -ne 0 ]; then
+		--repo "$TFACTION_DRIFT_ISSUE_REPO_OWNER/$TFACTION_DRIFT_ISSUE_REPO_NAME" \
+		"$TFACTION_DRIFT_ISSUE_NUMBER"
+elif [ "$TFACTION_DRIFT_ISSUE_STATE" = closed ] && [ "$code" -ne 0 ]; then
 	gh issue reopen \
-		--repo "$ISSUE_REPO_OWNER/$ISSUE_REPO_NAME" \
-		"$ISSUE_NUMBER"
+		--repo "$TFACTION_DRIFT_ISSUE_REPO_OWNER/$TFACTION_DRIFT_ISSUE_REPO_NAME" \
+		"$TFACTION_DRIFT_ISSUE_NUMBER"
 fi
