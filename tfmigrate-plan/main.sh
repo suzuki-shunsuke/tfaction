61ff7c1672b666b63fd58130b4b92b91196813da
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-09-24 10:49:54 +0900

    fix(tfmigrate-plan): stop removing plan files by tfmigrate-plan

--- tfmigrate-plan/main.sh
+++ tfmigrate-plan/main.sh
@@ -3,14 +3,6 @@
 set -eu
 set -o pipefail
 
-echo "===> Delete old plan file to prevent the accident" >&2
-if [ -n "${S3_BUCKET_NAME_PLAN_FILE:-}" ]; then
-	aws s3 rm "s3://$S3_BUCKET_NAME_PLAN_FILE/$PR_NUMBER/$TFACTION_TARGET/tfplan.binary" || :
-fi
-if [ -n "${GCS_BUCKET_NAME_PLAN_FILE:-}" ]; then
-	gsutil rm "gs://$GCS_BUCKET_NAME_PLAN_FILE/$PR_NUMBER/$TFACTION_TARGET/tfplan.binary" || :
-fi
-
 if [ ! -f .tfmigrate.hcl ]; then
 	if [ -n "${S3_BUCKET_NAME_TFMIGRATE_HISTORY:-}" ]; then
 		sed "s|%%TARGET%%|$TFACTION_TARGET|g" \
