4810c9d6df1a9d743c984795802d5af0e35b6ba6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-01-27 21:22:04 +0900

    style: format code with prettier
    ```sh
    npm run fmt
    ```

--- check-terraform-skip/src/index.ts
+++ check-terraform-skip/src/index.ts
@@ -1,19 +1,25 @@
-import * as core from '@actions/core';
-import * as fs from 'fs';
-import * as lib from './lib';
+import * as core from "@actions/core";
+import * as fs from "fs";
+import * as lib from "./lib";
 
 function getSkipTerraform(): boolean {
   const config = lib.getConfig();
-  const renovateLogin = config.renovate_login ? config.renovate_login : 'renovate[bot]';
-  const skipLabelPrefix = core.getInput('skip_label_prefix', { required: true });
-  const labels = fs.readFileSync(core.getInput('labels', { required: true }), 'utf8').split('\n');
+  const renovateLogin = config.renovate_login
+    ? config.renovate_login
+    : "renovate[bot]";
+  const skipLabelPrefix = core.getInput("skip_label_prefix", {
+    required: true,
+  });
+  const labels = fs
+    .readFileSync(core.getInput("labels", { required: true }), "utf8")
+    .split("\n");
   const target = process.env.TFACTION_TARGET;
   if (!target) {
-    throw 'TFACTION_TARGET is required';
+    throw "TFACTION_TARGET is required";
   }
-  if (renovateLogin != core.getInput('pr_author', { required: true })) {
+  if (renovateLogin != core.getInput("pr_author", { required: true })) {
     for (let i = 0; i < labels.length; i++) {
-      if (labels[i] == `${skipLabelPrefix}${target}` ) {
+      if (labels[i] == `${skipLabelPrefix}${target}`) {
         return true;
       }
     }
@@ -22,7 +28,11 @@ function getSkipTerraform(): boolean {
   if (!config.skip_terraform_by_renovate) {
     return false;
   }
-  const renovateTerraformLabels = new Set(config.renovate_terraform_labels ? config.renovate_terraform_labels : ['terraform']);
+  const renovateTerraformLabels = new Set(
+    config.renovate_terraform_labels
+      ? config.renovate_terraform_labels
+      : ["terraform"],
+  );
   for (let i = 0; i < labels.length; i++) {
     const label = labels[i];
     if (renovateTerraformLabels.has(label)) {
@@ -35,8 +45,10 @@ function getSkipTerraform(): boolean {
 
 try {
   const isSkip = getSkipTerraform();
-  core.exportVariable('TFACTION_SKIP_TERRAFORM', isSkip);
-  core.setOutput('skip_terraform', isSkip);
+  core.exportVariable("TFACTION_SKIP_TERRAFORM", isSkip);
+  core.setOutput("skip_terraform", isSkip);
 } catch (error) {
-  core.setFailed(error instanceof Error ? error.message : JSON.stringify(error));
+  core.setFailed(
+    error instanceof Error ? error.message : JSON.stringify(error),
+  );
 }
--- list-targets-with-changed-files/src/index.ts
+++ list-targets-with-changed-files/src/index.ts
@@ -1,18 +1,23 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as lib from './lib';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as fs from "fs";
+import * as path from "path";
+import * as lib from "./lib";
 
 interface TargetConfig {
-  target: string
-  runs_on: string
-  job_type: string
-  environment: string | object | null
-  secrets: object | undefined
+  target: string;
+  runs_on: string;
+  job_type: string;
+  environment: string | object | null;
+  secrets: object | undefined;
 }
 
-function getTargetConfigByTarget(targets: Array<lib.TargetConfig>, target: string, isApply: boolean, jobType: string): TargetConfig {
+function getTargetConfigByTarget(
+  targets: Array<lib.TargetConfig>,
+  target: string,
+  isApply: boolean,
+  jobType: string,
+): TargetConfig {
   for (let i = 0; i < targets.length; i++) {
     const t = targets[i];
     if (!target.startsWith(t.target)) {
@@ -22,7 +27,7 @@ function getTargetConfigByTarget(targets: Array<lib.TargetConfig>, target: strin
     if (jobConfig == undefined) {
       return {
         target: target,
-        runs_on: t.runs_on ? t.runs_on : 'ubuntu-latest',
+        runs_on: t.runs_on ? t.runs_on : "ubuntu-latest",
         environment: t.environment ? t.environment : null,
         secrets: t.secrets,
         job_type: jobType,
@@ -30,26 +35,36 @@ function getTargetConfigByTarget(targets: Array<lib.TargetConfig>, target: strin
     }
     return {
       target: target,
-      runs_on: jobConfig.runs_on ? jobConfig.runs_on : (t.runs_on ? t.runs_on : 'ubuntu-latest'),
-      environment: jobConfig.environment ? jobConfig.environment : (t.environment ? t.environment : null),
+      runs_on: jobConfig.runs_on
+        ? jobConfig.runs_on
+        : t.runs_on
+          ? t.runs_on
+          : "ubuntu-latest",
+      environment: jobConfig.environment
+        ? jobConfig.environment
+        : t.environment
+          ? t.environment
+          : null,
       secrets: jobConfig.secrets ? jobConfig.secrets : t.secrets,
       job_type: jobType,
     };
   }
-  throw 'target is invalid';
+  throw "target is invalid";
 }
 
 function getPRBody(): string {
   if (github.context.payload.pull_request) {
-    return github.context.payload.pull_request.body ? github.context.payload.pull_request.body : '';
+    return github.context.payload.pull_request.body
+      ? github.context.payload.pull_request.body
+      : "";
   }
-  const prPath = core.getInput('pull_request');
+  const prPath = core.getInput("pull_request");
   if (!prPath) {
-    return '';
+    return "";
   }
-  const pr = JSON.parse(fs.readFileSync(prPath, 'utf8'));
+  const pr = JSON.parse(fs.readFileSync(prPath, "utf8"));
   if (!pr || !pr.body) {
-    return '';
+    return "";
   }
   return pr.body;
 }
@@ -67,24 +82,30 @@ try {
     configTargetMap.set(target.target, target);
   }
 
-  const labels = fs.readFileSync(core.getInput('labels'), 'utf8').split('\n');
-  const changedFiles = fs.readFileSync(core.getInput('changed_files'), 'utf8').split('\n');
-  const configFiles = fs.readFileSync(core.getInput('config_files'), 'utf8').split('\n');
+  const labels = fs.readFileSync(core.getInput("labels"), "utf8").split("\n");
+  const changedFiles = fs
+    .readFileSync(core.getInput("changed_files"), "utf8")
+    .split("\n");
+  const configFiles = fs
+    .readFileSync(core.getInput("config_files"), "utf8")
+    .split("\n");
   const workingDirs = new Set<string>();
   for (let i = 0; i < configFiles.length; i++) {
     const configFile = configFiles[i];
-    if (configFile == '') {
+    if (configFile == "") {
       continue;
     }
     workingDirs.add(path.dirname(configFile));
   }
 
   // <!-- tfaction follow up pr target=foo -->
-  let followupTarget = '';
-  const followupPRBodyPrefix='<!-- tfaction follow up pr target=';
+  let followupTarget = "";
+  const followupPRBodyPrefix = "<!-- tfaction follow up pr target=";
   const prBody = getPRBody();
   if (prBody.startsWith(followupPRBodyPrefix)) {
-    followupTarget = prBody.split('\n')[0].slice(followupPRBodyPrefix.length, - ' -->'.length);
+    followupTarget = prBody
+      .split("\n")[0]
+      .slice(followupPRBodyPrefix.length, -" -->".length);
   }
 
   const terraformTargets = new Set<string>();
@@ -93,23 +114,42 @@ try {
   const terraformTargetObjs = new Array<TargetConfig>();
   const tfmigrateObjs = new Array<TargetConfig>();
 
-  const targetPrefix = (config.label_prefixes != undefined && config.label_prefixes.target != undefined && config.label_prefixes.target != '') ?
-    config.label_prefixes.target : 'target:';
-  const skipPrefix = (config.label_prefixes != undefined && config.label_prefixes.skip != undefined && config.label_prefixes.skip != '') ?
-    config.label_prefixes.skip : 'skip:';
-  const tfmigratePrefix = (config.label_prefixes != undefined && config.label_prefixes.tfmigrate != undefined && config.label_prefixes.tfmigrate != '') ?
-    config.label_prefixes.tfmigrate : 'tfmigrate:';
+  const targetPrefix =
+    config.label_prefixes != undefined &&
+    config.label_prefixes.target != undefined &&
+    config.label_prefixes.target != ""
+      ? config.label_prefixes.target
+      : "target:";
+  const skipPrefix =
+    config.label_prefixes != undefined &&
+    config.label_prefixes.skip != undefined &&
+    config.label_prefixes.skip != ""
+      ? config.label_prefixes.skip
+      : "skip:";
+  const tfmigratePrefix =
+    config.label_prefixes != undefined &&
+    config.label_prefixes.tfmigrate != undefined &&
+    config.label_prefixes.tfmigrate != ""
+      ? config.label_prefixes.tfmigrate
+      : "tfmigrate:";
 
   for (let i = 0; i < labels.length; i++) {
     const label = labels[i];
-    if (label == '') {
+    if (label == "") {
       continue;
     }
     if (label.startsWith(targetPrefix)) {
       const target = label.slice(targetPrefix.length);
       if (!terraformTargets.has(target)) {
         terraformTargets.add(target);
-        terraformTargetObjs.push(getTargetConfigByTarget(config.target_groups, target, isApply, 'terraform'));
+        terraformTargetObjs.push(
+          getTargetConfigByTarget(
+            config.target_groups,
+            target,
+            isApply,
+            "terraform",
+          ),
+        );
       }
       continue;
     }
@@ -117,7 +157,14 @@ try {
       const target = label.slice(tfmigratePrefix.length);
       if (!tfmigrates.has(target)) {
         tfmigrates.add(target);
-        tfmigrateObjs.push(getTargetConfigByTarget(config.target_groups, target, isApply, 'tfmigrate'));
+        tfmigrateObjs.push(
+          getTargetConfigByTarget(
+            config.target_groups,
+            target,
+            isApply,
+            "tfmigrate",
+          ),
+        );
       }
       continue;
     }
@@ -130,11 +177,11 @@ try {
   const changedWorkingDirs = new Set<string>();
   for (let i = 0; i < changedFiles.length; i++) {
     const changedFile = changedFiles[i];
-    if (changedFile == '') {
+    if (changedFile == "") {
       continue;
     }
     for (let workingDir of workingDirs) {
-      if (changedFile.startsWith(workingDir + '/')) {
+      if (changedFile.startsWith(workingDir + "/")) {
         changedWorkingDirs.add(workingDir);
       }
     }
@@ -144,22 +191,48 @@ try {
     for (let i = 0; i < config.target_groups.length; i++) {
       const target = config.target_groups[i];
       if (changedWorkingDir.startsWith(target.working_directory)) {
-        const changedTarget = changedWorkingDir.replace(target.working_directory, target.target);
-        if (!terraformTargets.has(changedTarget) && !tfmigrates.has(changedTarget)) {
+        const changedTarget = changedWorkingDir.replace(
+          target.working_directory,
+          target.target,
+        );
+        if (
+          !terraformTargets.has(changedTarget) &&
+          !tfmigrates.has(changedTarget)
+        ) {
           terraformTargets.add(changedTarget);
-          terraformTargetObjs.push(getTargetConfigByTarget(config.target_groups, changedTarget, isApply, 'terraform'));
+          terraformTargetObjs.push(
+            getTargetConfigByTarget(
+              config.target_groups,
+              changedTarget,
+              isApply,
+              "terraform",
+            ),
+          );
         }
         break;
       }
     }
   }
 
-  if (followupTarget && !tfmigrates.has(followupTarget) && !terraformTargets.has(followupTarget)) {
+  if (
+    followupTarget &&
+    !tfmigrates.has(followupTarget) &&
+    !terraformTargets.has(followupTarget)
+  ) {
     terraformTargets.add(followupTarget);
-    terraformTargetObjs.push(getTargetConfigByTarget(config.target_groups, followupTarget, isApply, 'terraform'));
+    terraformTargetObjs.push(
+      getTargetConfigByTarget(
+        config.target_groups,
+        followupTarget,
+        isApply,
+        "terraform",
+      ),
+    );
   }
 
-  core.setOutput('targets', terraformTargetObjs.concat(tfmigrateObjs));
+  core.setOutput("targets", terraformTargetObjs.concat(tfmigrateObjs));
 } catch (error) {
-  core.setFailed(error instanceof Error ? error.message : JSON.stringify(error));
+  core.setFailed(
+    error instanceof Error ? error.message : JSON.stringify(error),
+  );
 }
