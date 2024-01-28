88c20d50661ef38740e231c472235ce6b80df5b9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-01-28 10:23:43 +0900

    style: format

--- check-terraform-skip/dist/index.js
+++ check-terraform-skip/dist/index.js
@@ -57963,15 +57963,69 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
-const fs = __importStar(__nccwpck_require__(7147));
+const lib = __importStar(__nccwpck_require__(9730));
+try {
+    lib.main();
+}
+catch (error) {
+    core.setFailed(error instanceof Error ? error.message : JSON.stringify(error));
+}
+
+
+/***/ }),
+
+/***/ 9730:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getSkipTerraform = exports.main = void 0;
 const lib = __importStar(__nccwpck_require__(8022));
-const getSkipTerraform = (inputs) => {
-    var _a, _b;
-    // https://suzuki-shunsuke.github.io/tfaction/docs/feature/support-skipping-terraform-renovate-pr
+const core = __importStar(__nccwpck_require__(2186));
+const fs = __importStar(__nccwpck_require__(7147));
+const main = () => {
+    const inputs = {
+        skipLabelPrefix: core.getInput("skip_label_prefix", { required: true }),
+        labels: core.getInput("labels", { required: true }),
+        prAuthor: core.getInput("pr_author", { required: true }),
+        target: process.env.TFACTION_TARGET,
+    };
     const config = lib.getConfig();
-    const renovateLogin = (_a = config.renovate_login) !== null && _a !== void 0 ? _a : "renovate[bot]";
     // labels is pull request's labels.
     const labels = fs.readFileSync(inputs.labels, "utf8").split("\n");
+    const isSkip = (0, exports.getSkipTerraform)(inputs, config, labels);
+    core.exportVariable("TFACTION_SKIP_TERRAFORM", isSkip);
+    core.setOutput("skip_terraform", isSkip);
+};
+exports.main = main;
+const getSkipTerraform = (inputs, config, labels) => {
+    var _a, _b;
+    // https://suzuki-shunsuke.github.io/tfaction/docs/feature/support-skipping-terraform-renovate-pr
+    const renovateLogin = (_a = config.renovate_login) !== null && _a !== void 0 ? _a : "renovate[bot]";
     if (!inputs.target) {
         throw new Error("TFACTION_TARGET is required");
     }
@@ -57998,19 +58052,7 @@ const getSkipTerraform = (inputs) => {
     }
     return true;
 };
-try {
-    const isSkip = getSkipTerraform({
-        skipLabelPrefix: core.getInput("skip_label_prefix", { required: true }),
-        labels: core.getInput("labels", { required: true }),
-        prAuthor: core.getInput("pr_author", { required: true }),
-        target: process.env.TFACTION_TARGET,
-    });
-    core.exportVariable("TFACTION_SKIP_TERRAFORM", isSkip);
-    core.setOutput("skip_terraform", isSkip);
-}
-catch (error) {
-    core.setFailed(error instanceof Error ? error.message : JSON.stringify(error));
-}
+exports.getSkipTerraform = getSkipTerraform;
 
 
 /***/ }),
