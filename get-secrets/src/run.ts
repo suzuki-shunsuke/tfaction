0166afa3a310c8ceeb72d48af04870cec6f6037a
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-06 21:12:07 +0900

    feat: add get-secrets

---
+++ get-secrets/src/run.ts
@@ -0,0 +1,93 @@
+import * as core from "@actions/core";
+import * as lib from "./lib";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+  GetSecretValueCommandInput,
+} from "@aws-sdk/client-secrets-manager";
+
+function exportSecret(
+  envName: string,
+  secretID: string,
+  secretValue: string,
+  secretKey: string,
+) {
+  if (secretKey) {
+    core.info(
+      `output the secret: secret_id=${secretID} env_name=${envName} secret_key=${secretKey}`,
+    );
+  } else {
+    core.info(`output the secret: secret_id=${secretID} env_name=${envName}`);
+  }
+  core.setSecret(secretValue);
+  core.setOutput(envName, secretValue);
+}
+
+async function exportSecrets(
+  client: SecretsManagerClient,
+  secrets: Array<lib.AWSSecretsManagerSecret>,
+) {
+  for (let i = 0; i < secrets.length; i++) {
+    const secret = secrets[i];
+    if (!secret.secret_id) {
+      throw "secret_id is required";
+    }
+    const command = new GetSecretValueCommand({
+      SecretId: secret.secret_id,
+    });
+    const response = await client.send(command);
+    if (!response.SecretString) {
+      throw `SecretString is empty: secret_id=${secret.secret_id}`;
+    }
+    let secretJSON = null;
+    const secretMap = new Map<string, string>();
+    for (let j = 0; j < secret.envs.length; j++) {
+      const e = secret.envs[j];
+      if (!e.env_name) {
+        throw `env_name is required: secret_id=${secret.secret_id}`;
+      }
+      if (!e.secret_key) {
+        secretMap.set(e.env_name, response.SecretString);
+        exportSecret(e.env_name, secret.secret_id, response.SecretString, "");
+        continue;
+      }
+      if (!secretJSON) {
+        secretJSON = JSON.parse(response.SecretString);
+      }
+      if (!secretJSON[e.secret_key]) {
+        throw `secret key isn't found: secret_key=${e.secret_key} secret_id=${secret.secret_id}`;
+      }
+      const secretValue = secretJSON[e.secret_key];
+      secretMap.set(e.env_name, secretValue);
+      exportSecret(e.env_name, secret.secret_id, secretValue, e.secret_key);
+    }
+    const secretMapJSON = JSON.stringify(secretMap);
+    core.setSecret(secretMapJSON);
+    core.setOutput("secrets", secretMapJSON);
+  }
+}
+
+export const run = async (): Promise<void> => {
+  const config = lib.getConfig();
+  const targetS = lib.getTarget();
+  const jobType = lib.getJobType();
+  const isApply = lib.getIsApply();
+  const targetConfig = lib.getTargetConfig(config.target_groups, targetS);
+  const jobConfig = lib.getJobConfig(targetConfig, isApply, jobType);
+  let awsClient = null;
+  if (targetConfig.aws_secrets_manager) {
+    awsClient = new SecretsManagerClient({
+      region: targetConfig.aws_region,
+    });
+    await exportSecrets(awsClient, targetConfig.aws_secrets_manager);
+  }
+
+  if (jobConfig && jobConfig.aws_secrets_manager) {
+    if (!awsClient) {
+      awsClient = new SecretsManagerClient({
+        region: targetConfig.aws_region,
+      });
+    }
+    await exportSecrets(awsClient, jobConfig.aws_secrets_manager);
+  }
+};
