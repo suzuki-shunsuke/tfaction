6af4f5aa1c3e6a6028a68642f7549f44616fd080
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-03-11 07:30:54 +0900

    fix: add commit hashes to an error message

--- terraform-apply/download_plan_file.sh
+++ terraform-apply/download_plan_file.sh
@@ -16,7 +16,7 @@ run_id=$(echo "$body" | jq -r ".databaseId")
 head_sha=$(echo "$body" | jq -r ".headSha")
 
 if [ "$head_sha" != "$pr_head_sha" ]; then
-	echo "::error::workflow run's headSha is different from the associated pull request's head sha"
+	echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
 	github-comment post \
 		-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
 		-k invalid-workflow-sha \
