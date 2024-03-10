552f24701ed18c80bdc6e84690343ddbe8f53b80
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-03-11 07:46:05 +0900

    test

--- terraform-apply/download_plan_file.sh
+++ terraform-apply/download_plan_file.sh
@@ -17,15 +17,13 @@ body=$(gh run list -w "$workflow" -b "$branch" -L 1 --json headSha,databaseId --
 run_id=$(echo "$body" | jq -r ".databaseId")
 head_sha=$(echo "$body" | jq -r ".headSha")
 
-if [ "$head_sha" != "$pr_head_sha" ]; then
-	echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
-	github-comment post \
-		-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
-		-k invalid-workflow-sha \
-		-var "wf_sha:$head_sha" \
-		-var "pr_sha:$pr_head_sha"
-	exit 1
-fi
+echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
+github-comment post \
+	-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
+	-k invalid-workflow-sha \
+	-var "wf_sha:$head_sha" \
+	-var "pr_sha:$pr_head_sha"
+exit 1
 
 tempdir=$(mktemp -d)
 
