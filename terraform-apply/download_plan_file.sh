76e71f160b62edf327130d07bb1afcdb7e3ef70f
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-03-30 12:30:01 +0900

    fix(terraform-apply): improve error handling

--- terraform-apply/download_plan_file.sh
+++ terraform-apply/download_plan_file.sh
@@ -13,23 +13,18 @@ pr_head_sha=$(jq -r ".head.sha" "$CI_INFO_TEMP_DIR/pr.json")
 # 1. this requires GitHub CLI v2.40.0 or newer
 # 2. we should check the latest workflow run
 body=$(github-comment exec \
+	-k list-workflow-runs \
 	-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
 	-var "tfaction_target:$TFACTION_TARGET" \
 	-- gh run list -w "$PLAN_WORKFLOW_NAME" -b "$branch" -L 1 --json headSha,databaseId --jq '.[0]')
+
+if [ -z "$body" ]; then
+	echo "::error::body is empty. No workflow run is found"
+	exit 1
+fi
+
 run_id=$(echo "$body" | jq -r ".databaseId")
 head_sha=$(echo "$body" | jq -r ".headSha")
-
-if [ -z "$head_sha" ]; then
-	if ! github-comment exec \
-		-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
-		-k invalid-plan-workflow-name \
-		-var "plan_workflow_name:$PLAN_WORKFLOW_NAME" \
-		-var "tfaction_target:$TFACTION_TARGET" \
-		-- gh workflow view "$PLAN_WORKFLOW_NAME"; then
-		echo "::error::Failed to view the workflow ($PLAN_WORKFLOW_NAME). Probably the setting plan_workflow_name in tfaction-root.yaml is wrong."
-		exit 1
-	fi
-fi
 
 if [ "$head_sha" != "$pr_head_sha" ]; then
 	echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
@@ -46,6 +41,7 @@ tempdir=$(mktemp -d)
 
 github-comment exec \
 	-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
+	-k download-plan-file \
 	-var "tfaction_target:$TFACTION_TARGET" \
 	-- gh run download -D "$tempdir" -n "$artifact_name" "$run_id"
 cp "$tempdir/$filename" tfplan.binary
