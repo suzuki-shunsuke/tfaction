89ecbfdb2ef6fb1001b5ffc8afdcafc17ff7f1fe
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-03-30 11:21:35 +0900

    fix(terraform-apply): output the good error message if plan_workflow_name is wrong

--- terraform-apply/download_plan_file.sh
+++ terraform-apply/download_plan_file.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 filename=tfplan.binary
 artifact_name=terraform_plan_file_${TFACTION_TARGET//\//__}
 branch=$CI_INFO_HEAD_REF
-workflow=$PLAN_WORKFLOW_NAME
 
 pr_head_sha=$(jq -r ".head.sha" "$CI_INFO_TEMP_DIR/pr.json")
 
@@ -13,9 +12,20 @@ pr_head_sha=$(jq -r ".head.sha" "$CI_INFO_TEMP_DIR/pr.json")
 # We don't use gh run list's -c option because 
 # 1. this requires GitHub CLI v2.40.0 or newer
 # 2. we should check the latest workflow run
-body=$(gh run list -w "$workflow" -b "$branch" -L 1 --json headSha,databaseId --jq '.[0]')
+body=$(gh run list -w "$PLAN_WORKFLOW_NAME" -b "$branch" -L 1 --json headSha,databaseId --jq '.[0]')
 run_id=$(echo "$body" | jq -r ".databaseId")
 head_sha=$(echo "$body" | jq -r ".headSha")
+
+if [ -z "$head_sha" ]; then
+	if ! github-comment exec \
+		-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
+		-k invalid-plan-workflow-name \
+		-var "plan_workflow_name:$PLAN_WORKFLOW_NAME" \
+		-- gh workflow view "$PLAN_WORKFLOW_NAME"; then
+		echo "::error::Failed to view the workflow ($PLAN_WORKFLOW_NAME). Probably the setting plan_workflow_name in tfaction-root.yaml is wrong."
+		exit 1
+	fi
+fi
 
 if [ "$head_sha" != "$pr_head_sha" ]; then
 	echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
