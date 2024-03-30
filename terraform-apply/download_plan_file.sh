503e4f93688a5f1ba7279ecd6947586a2a64c04d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-03-30 12:44:02 +0900

    fix(terraform-apply): pass github-comment variables via environment variables

--- terraform-apply/download_plan_file.sh
+++ terraform-apply/download_plan_file.sh
@@ -8,14 +8,16 @@ branch=$CI_INFO_HEAD_REF
 
 pr_head_sha=$(jq -r ".head.sha" "$CI_INFO_TEMP_DIR/pr.json")
 
+export GH_COMMENT_CONFIG=${GITHUB_ACTION_PATH}/github-comment.yaml
+export GH_COMMENT_VAR_tfaction_target=$TFACTION_TARGET
+export GH_COMMENT_VAR_plan_workflow_name=$PLAN_WORKFLOW_NAME
+
 # https://github.com/suzuki-shunsuke/tfaction/pull/1570#issuecomment-1987382651
 # We don't use gh run list's -c option because 
 # 1. this requires GitHub CLI v2.40.0 or newer
 # 2. we should check the latest workflow run
 body=$(github-comment exec \
 	-k list-workflow-runs \
-	-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
-	-var "tfaction_target:$TFACTION_TARGET" \
 	-- gh run list -w "$PLAN_WORKFLOW_NAME" -b "$branch" -L 1 --json headSha,databaseId --jq '.[0]')
 
 if [ -z "$body" ]; then
@@ -29,9 +31,7 @@ head_sha=$(echo "$body" | jq -r ".headSha")
 if [ "$head_sha" != "$pr_head_sha" ]; then
 	echo "::error::workflow run's headSha ($head_sha) is different from the associated pull request's head sha ($pr_head_sha)"
 	github-comment post \
-		-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
 		-k invalid-workflow-sha \
-		-var "tfaction_target:$TFACTION_TARGET" \
 		-var "wf_sha:$head_sha" \
 		-var "pr_sha:$pr_head_sha"
 	exit 1
@@ -40,8 +40,6 @@ fi
 tempdir=$(mktemp -d)
 
 github-comment exec \
-	-config "${GITHUB_ACTION_PATH}/github-comment.yaml" \
 	-k download-plan-file \
-	-var "tfaction_target:$TFACTION_TARGET" \
 	-- gh run download -D "$tempdir" -n "$artifact_name" "$run_id"
 cp "$tempdir/$filename" tfplan.binary
