d7251d698e3d4cc018f47a85f59e41e161b7419e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-09-20 11:11:28 +0900

    fix: fix the file name and change links to pull request urls

--- create-follow-up-pr/create_commit.sh
+++ create-follow-up-pr/create_commit.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 if [ "${CREATE_FOLLOW_UP_PR_FILE:-}" = "true" ]; then
-	follow_up_pr_file="$WORKING_DIR/.tfaction-follow-up-pr"
+	follow_up_pr_file="$WORKING_DIR/.tfaction-failed-prs"
 	if [ ! -f "$follow_up_pr_file" ]; then
-		echo "# This file is created and updated for follow up pull requests.
+		echo "# This file is created and updated by tfaction for follow up pull requests.
 # You can remove this file safely." > "$follow_up_pr_file"
 	fi
-	echo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" >> "$follow_up_pr_file"
+	echo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/pull/$CI_INFO_PR_NUMBER" >> "$follow_up_pr_file"
 	ghcp commit \
 		-r "$GITHUB_REPOSITORY" -b "$FOLLOW_UP_BRANCH" \
 		-m "chore: create a commit to open follow up pull request
