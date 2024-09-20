fb12a2c9fa96cf51d23d007386bf8f2e43585751
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-09-20 12:31:48 +0900

    fix: fix skip_create_pr.sh

--- create-follow-up-pr/skip_create_pr.sh
+++ create-follow-up-pr/skip_create_pr.sh
@@ -5,20 +5,12 @@ set -euo pipefail
 # create a branch with empty commit
 # 1. create a remote branch
 
-target_label=${TFACTION_TARGET_LABEL_PREFIX}${TFACTION_TARGET}
-# Create a label in advance because if the label doesn't exist gh pr create command fails
-gh api "repos/{owner}/{repo}/labels" \
-	-f name="${target_label}" || :
-
-FOLLOW_UP_BRANCH="follow-up-$CI_INFO_PR_NUMBER-$TFACTION_TARGET-$(date +%Y%m%dT%H%M%S)"
+export FOLLOW_UP_BRANCH="follow-up-$CI_INFO_PR_NUMBER-$TFACTION_TARGET-$(date +%Y%m%dT%H%M%S)"
 bash "$GITHUB_ACTION_PATH/create_commit.sh"
-
-Follow up #$CI_INFO_PR_NUMBER
-$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
 pr_title="chore($TFACTION_TARGET): follow up #$CI_INFO_PR_NUMBER"
 
-create_opts=( -l "\"$target_label\"" -R "$GITHUB_REPOSITORY" -H "$FOLLOW_UP_BRANCH" -t "\"$pr_title\"" -b "\"Follow up #$CI_INFO_PR_NUMBER\"" )
+create_opts=( -R "$GITHUB_REPOSITORY" -H "$FOLLOW_UP_BRANCH" -t "\"$pr_title\"" -b "\"Follow up #$CI_INFO_PR_NUMBER\"" )
 mention=""
 if ! [[ "$CI_INFO_PR_AUTHOR" =~ \[bot\] ]]; then
 	mention="@$CI_INFO_PR_AUTHOR"
