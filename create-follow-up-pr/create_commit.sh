e0ebb14c50faddd1faf4483dfed8b50ec69ad899
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-09-20 09:48:22 +0900

    fix: fix the path of .tfaction-follow-up-pr

--- create-follow-up-pr/create_commit.sh
+++ create-follow-up-pr/create_commit.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [ "${CREATE_FOLLOW_UP_PR_FILE:-}" = "true" ]; then
-	follow_up_pr_file=.tfaction-follow-up-pr
+	follow_up_pr_file="$WORKING_DIR/.tfaction-follow-up-pr"
 	if [ ! -f "$follow_up_pr_file" ]; then
 		echo "# This file is created and updated for follow up pull requests.
 # You can remove this file safely." > "$follow_up_pr_file"
@@ -14,6 +14,7 @@ if [ "${CREATE_FOLLOW_UP_PR_FILE:-}" = "true" ]; then
 		-m "chore: create a commit to open follow up pull request
 Follow up #$CI_INFO_PR_NUMBER
 $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" \
+        -C "$GITHUB_WORKSPACE" \
 		"$follow_up_pr_file"
 else
 	ghcp empty-commit \
