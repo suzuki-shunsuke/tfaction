751e1a283a016beb21e544fc0f8ebdd3271cfeb8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-10-08 21:38:16 +0900

    fix(conftest-hcl): fix a broken shell script

--- conftest-hcl/main.sh
+++ conftest-hcl/main.sh
@@ -6,7 +6,7 @@ shopt -s nullglob # To ignore *.tf and *.tf.json when no files are found
 conftest -v # Install conftest in advance to exclude aqua lazy install log from github-comment's comment
 
 if [ -n "$CONFTEST_POLICY_DIRECTORY" ]; then
-	[ ! -d "$ROOT_DIR/$CONFTEST_POLICY_DIRECTORY" ]; then
+	if [ ! -d "$ROOT_DIR/$CONFTEST_POLICY_DIRECTORY" ]; then
 		echo "::error::The conftest directory $CONFTEST_POLICY_DIRECTORY isn't found"
 		exit 1
 	fi
@@ -18,7 +18,7 @@ if [ -n "$CONFTEST_POLICY_DIRECTORY" ]; then
 fi
 
 if [ -n "$CONFTEST_COMBINE_POLICY_DIRECTORY" ]; then
-	[ ! -d "$ROOT_DIR/$CONFTEST_COMBINE_POLICY_DIRECTORY" ]; then
+	if [ ! -d "$ROOT_DIR/$CONFTEST_COMBINE_POLICY_DIRECTORY" ]; then
 		echo "::error::The conftest directory $CONFTEST_COMBINE_POLICY_DIRECTORY isn't found"
 		exit 1
 	fi
