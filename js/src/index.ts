f940205356efa4c705022f0d93e26e9e2f504709
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-20 14:31:06 +0900

    ci: test actions' outputs

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,10 +1,15 @@
 import * as core from "@actions/core";
 import { main } from "./run";
+import * as testAction from "./test-action";
 
 try {
-  main({
-    action: core.getInput("action"),
-  });
+  if (process.env.TFACTION_TEST_ACTION) {
+    testAction.main();
+  } else {
+    main({
+      action: core.getInput("action"),
+    });
+  }
 } catch (error) {
   core.setFailed(
     error instanceof Error ? error.message : JSON.stringify(error),
---
+++ js/src/test-action/index.ts
@@ -0,0 +1,184 @@
+import * as lib from "../lib";
+import * as core from "@actions/core";
+import * as fs from "fs";
+import { diffString } from "json-diff";
+
+export const main = async () => {
+  // Compare outputs
+  const testdata = [
+    {
+      name: "get-target-config",
+      expected: {
+        working_directory: "js/test/aws/foo/dev",
+        aws_assume_role_arn:
+          "arn:aws:iam::000000000000:role/GitHubActions_Terraform_AWS_terraform_plan",
+        aws_region: "ap-northeast-1",
+        aws_role_session_name: "tfaction-plan-js_test_aws_foo_dev-12425638978",
+        gcp_service_account: "",
+        gcp_workload_identity_provider: "",
+        gcp_access_token_scopes: "",
+        s3_bucket_name_tfmigrate_history:
+          "0000-0000-0000-suzuki-shunsuke-tfmigrate-history",
+        gcs_bucket_name_tfmigrate_history: "",
+        template_dir: "js/test/templates/aws",
+        providers_lock_opts:
+          "-platform=windows_amd64 -platform=linux_amd64 -platform=darwin_amd64",
+        enable_tfsec: "false",
+        enable_tflint: "true",
+        enable_trivy: "true",
+        enable_terraform_docs: "false",
+        destroy: "false",
+        terraform_command: "terraform",
+        tflint_fix: "true",
+      },
+      actual: process.env.GET_TARGET_CONFIG,
+    },
+    {
+      name: "js/get-target-config",
+      expected: {
+        working_directory: "js/test/aws/foo/dev",
+        providers_lock_opts:
+          "-platform=windows_amd64 -platform=linux_amd64 -platform=darwin_amd64",
+        template_dir: "js/test/templates/aws",
+        enable_tfsec: "false",
+        enable_tflint: "true",
+        enable_trivy: "true",
+        tflint_fix: "true",
+        terraform_command: "terraform",
+        s3_bucket_name_tfmigrate_history:
+          "0000-0000-0000-suzuki-shunsuke-tfmigrate-history",
+        aws_region: "ap-northeast-1",
+        aws_assume_role_arn:
+          "arn:aws:iam::000000000000:role/GitHubActions_Terraform_AWS_terraform_plan",
+        aws_role_session_name: "tfaction-plan-js_test_aws_foo_dev-12425638978",
+        destroy: "false",
+        enable_terraform_docs: "false",
+      },
+      actual: process.env.JS_TARGET_CONFIG,
+    },
+    {
+      name: "get-global-config",
+      expected: {
+        base_working_directory: ".",
+        module_base_directory: ".",
+        working_directory_file: "tfaction.yaml",
+        module_file: "tfaction_module.yaml",
+        renovate_login: "renovate[bot]",
+        label_prefix_target: "target:",
+        label_prefix_tfmigrate: "migrate:",
+        label_prefix_skip: "skip:",
+        skip_create_pr: "false",
+        drift_issue_repo_owner: "suzuki-shunsuke",
+        drift_issue_repo_name: "tfaction",
+        enable_tfsec: "false",
+        enable_tflint: "true",
+        enable_trivy: "true",
+        update_local_path_module_caller: "true",
+        aqua_update_checksum_enabled: "true",
+        aqua_update_checksum_skip_push: "false",
+        aqua_update_checksum_prune: "true",
+        plan_workflow_name: "test",
+        terraform_command: "terraform",
+      },
+      actual: process.env.GET_GLOBAL_CONFIG,
+    },
+    {
+      name: "js/get-global-config",
+      expected: {
+        base_working_directory: ".",
+        working_directory_file: "tfaction.yaml",
+        module_base_directory: ".",
+        module_file: "tfaction_module.yaml",
+        renovate_login: "renovate[bot]",
+        draft_pr: "false",
+        skip_create_pr: "false",
+        plan_workflow_name: "test",
+        label_prefix_target: "target:",
+        label_prefix_tfmigrate: "migrate:",
+        label_prefix_skip: "skip:",
+        disable_update_related_pull_requests: "false",
+        update_local_path_module_caller: "true",
+        aqua_update_checksum_enabled: "true",
+        aqua_update_checksum_prune: "true",
+        aqua_update_checksum_skip_push: "false",
+        enable_tfsec: "false",
+        enable_tflint: "true",
+        enable_trivy: "true",
+        terraform_command: "terraform",
+        drift_issue_repo_owner: "suzuki-shunsuke",
+        drift_issue_repo_name: "tfaction",
+      },
+      actual: process.env.JS_GLOBAL_CONFIG,
+    },
+    {
+      name: "check-terraform-skip",
+      expected: {
+        skip_terraform: "false",
+      },
+      actual: process.env.CHECK_TERRAFORM_SKIP,
+    },
+    {
+      name: "js/check-terraform-skip",
+      expected: {
+        skip_terraform: "false",
+      },
+      actual: process.env.JS_CHECK_TERRAFORM_SKIP,
+    },
+    {
+      name: "list-changed-modules",
+      expected: {
+        modules: '["js/test/modules/foo"]',
+      },
+      actual: process.env.LIST_CHANGED_MODULES,
+    },
+    {
+      name: "js/list-changed-modules",
+      expected: {
+        modules: '["js/test/modules/foo"]',
+      },
+      actual: process.env.JS_LIST_CHANGED_MODULES,
+    },
+    {
+      name: "list-module-callers",
+      expected: {
+        file: '{"js/test/modules/foo":["js/test/aws/foo/dev"]}',
+      },
+      actual: process.env.LIST_MODULE_CALLERS,
+    },
+    {
+      name: "js/list-module-callers",
+      expected: {
+        file: '{"js/test/modules/foo":["js/test/aws/foo/dev"]}',
+      },
+      actual: process.env.JS_LIST_MODULE_CALLERS,
+    },
+    {
+      name: "list-targets-with-changed-files",
+      expected: {
+        targets: "[]",
+        modules: '["js/test/modules/foo"]',
+      },
+      actual: process.env.LIST_TARGETS_WITH_CHANGED_FILES,
+    },
+    {
+      name: "js/list-targets-with-changed-files",
+      expected: {
+        targets: "[]",
+        modules: '["js/test/modules/foo"]',
+      },
+      actual: process.env.JS_LIST_TARGETS_WITH_CHANGED_FILES,
+    },
+  ];
+  let failed = false;
+  testdata.forEach((data) => {
+    const a = diffString(data.expected, data.actual);
+    if (a !== "") {
+      console.log(`Test failed: ${data.name}`);
+      console.log(a);
+      failed = true;
+    }
+  });
+  if (failed) {
+    throw new Error("Test failed");
+  }
+};
