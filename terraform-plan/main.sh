395a0429d7f59918381761777727d63293137233
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-07-03 14:30:22 +0900

    feat: output detailed-exitcode and plan file

--- terraform-plan/main.sh
+++ terraform-plan/main.sh
@@ -20,12 +20,18 @@ tfcmt -var "target:$TFACTION_TARGET" -var "destroy:${DESTROY:-}" plan -- \
 	"$TF_COMMAND" plan -no-color -detailed-exitcode -out "${PWD}/tfplan.binary" -input=false $opts
 code=$?
 set -e
+echo "detailed_exitcode=$code" >> "$GITHUB_OUTPUT"
+tempdir=$(mktemp -d)
+cp tfplan.binary "$tempdir/tfplan.binary"
+echo "plan_binary=${tempdir}/tfplan.binary" >> "$GITHUB_OUTPUT"
 
 if [ "$code" -eq 1 ]; then
 	exit 1
 fi
 
 github-comment exec -- "$TF_COMMAND" show -json tfplan.binary >tfplan.json
+cp tfplan.json "$tempdir/tfplan.json"
+echo "plan_json=${tempdir}/tfplan.json" >> "$GITHUB_OUTPUT"
 
 if [ "$code" = "0" ]; then
 	exit 0
