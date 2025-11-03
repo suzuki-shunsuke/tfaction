37122f15b905f1950db7ed92aeeebdeaf76a62aa
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-11-03 15:33:29 +0900

    fix: output urls

--- terraform-apply/update_branches.sh
+++ terraform-apply/update_branches.sh
@@ -11,6 +11,6 @@ echo "$PR_NUMBERS" | while read -r pr_number; do
 		# To prevent infinite loop
 		continue
 	fi
-	echo "::notice:: Update PR $pr_number" >&2
+	echo "::notice:: Update PR ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/$pr_number" >&2
 	gh api -X PUT "repos/{owner}/{repo}/pulls/${pr_number}/update-branch" || :
 done
